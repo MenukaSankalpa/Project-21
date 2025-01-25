e73bfb9b187d2bb702c2db06f02f0aee123b5f6e
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-25 14:17:34 +0530

    update js file

---
+++ index.js
@@ -0,0 +1,6 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
+const winConditions = [
+    [],
+];
