4600b39d927db286418ab72e078c81a631bc1d59
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-26 10:02:34 +0530

    update initializeGame and cellClicked

--- index.js
+++ index.js
@@ -15,13 +15,30 @@ let options = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
 let running = false;
 
+initializeGame();
+
 function initializeGame(){
+    cells.forEach((cell) => cell.addEventListener("click", cellClicked));
+    restartBtn.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
 
 }
 function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if(options[cellIndex] !== "" || !running) {
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner();
 
 }
-function updateCell(){
+function updateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+
 
 }
 function changePlayer(){
@@ -31,5 +48,5 @@ function checkWinner(){
 
 }
 function restartGame(){
-    
+
 }
